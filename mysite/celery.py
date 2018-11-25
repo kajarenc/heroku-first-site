24111dabcc38a117ed781fe3cb5567b20129aa54
Karen Javadyan <karen@teamable.com>
2018-11-25 19:39:24 +0400

    try to fix heroku celery issue

--- mysite/celery.py
+++ mysite/celery.py
@@ -13,8 +13,11 @@ app = Celery('mysite')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+app.conf.update(CELERY_BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                RESULT_BACKEND=os.environ['REDIS_URL'])
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
