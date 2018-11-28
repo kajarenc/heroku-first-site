0f46137572d45988bdbaac2fc0e523eebc08cb59
Karen Javadyan <karen@teamable.com>
2018-11-28 17:42:41 +0400

    experements in celery.py

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 # set the default Django settings module for the 'celery' program.
@@ -24,3 +25,51 @@ def debug_task(self):
 @app.task
 def zoom(self):
     return "Zooooooooooooooooooooooooooooooooooooooom"
+
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+@app.task
+def test(arg):
+    print(arg)
+
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'polls.tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
+
+
+
+app.conf.beat_schedule = {
+    'add-every-minute-contrab': {
+        'task': 'multiply_two_numbers',
+        'schedule': crontab(),
+        'args': (16, 16),
+    },
+    'add-every-5-seconds': {
+        'task': 'multiply_two_numbers',
+        'schedule': 5.0,
+        'args': (16, 16)
+    },
+    'add-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
