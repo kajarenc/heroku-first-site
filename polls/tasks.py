d196e0e901688ace18162dd749cb9181eaa6c340
Karen Javadyan <karen@teamable.com>
2018-11-28 16:26:32 +0400

    add self as a arg for bind celery tasks

--- polls/tasks.py
+++ polls/tasks.py
@@ -3,14 +3,14 @@ from mysite.celery import app
 
 
 @app.task(bind=True)
-def add(x, y):
+def add(self, x, y):
     return x + y
 
 @app.task(bind=True)
-def mul(x, y):
+def mul(self, x, y):
     total = x * (y * random.randint(3, 100))
     return total
 
 @app.task(bind=True)
-def xsum(numbers):
+def xsum(self, numbers):
     return sum(numbers)
